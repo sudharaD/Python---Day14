109d7476438d276b76f86561e6420b9d5c932bce
sudharaD <sudharad7@gmail.com>
2023-01-11 23:01:58 +0530

    figure out random pickings

--- higher_lower_game.py
+++ higher_lower_game.py
@@ -1,2 +1,11 @@
 import art
-from game_data import game_data
+from game_data import data
+import random
+
+# Generate Logo
+# Compare A
+# vs art
+# Against B
+# Get the answer from user
+
+print(random.choice(data)['name'])
