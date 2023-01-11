6c7ebeb8dabfa7018a5326ca1bb4066d1ee817c7
sudharaD <sudharad7@gmail.com>
2023-01-11 23:26:09 +0530

    necessary comments add

--- higher_lower_game.py
+++ higher_lower_game.py
@@ -3,9 +3,25 @@ from game_data import data
 import random
 
 # Generate Logo
-# Compare A
+
+# random_picker function - take the celebrety list as a attribute and randomely picked a item in the list and return it
+
+# First time pick the item randomely and stored in 2 different variables - (Call random_picker function)
+# Random pick A
 # vs art
-# Against B
+# Random pick B
+
 # Get the answer from user
+
+# compare follower function - take 2 dictionaries as attributes and return the dictionary with most follower count
+
+# call compare_follower function to compare the follower count
+
+# if user answer right answer dictionary_b saved to dictionary_a variable and call the random_picker function and save the dictionary in dictionary_b variable
+# And score increase by 1
+# clear()
+# repeat the process
+
+# if user wrong - clear(), message, score
 
 print(random.choice(data)['name'])
